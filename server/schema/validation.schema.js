f2ea88b7f22078b709b44cda49b79019a611d547
user-ben-001 <mantrand.benjamin@gmail.com>
2026-05-18 15:51:54 +0200

    fix : schema params

--- server/schema/validation.schema.js
+++ server/schema/validation.schema.js
@@ -2,13 +2,19 @@ import Joi from "joi";
 
 export const loginSchema = Joi.object({
   email: Joi.string().email().required(),
-  password: Joi.string().min(6).pattern(/[a-zA-Z0-9]/).required(),
+  password: Joi.string()
+    .min(6)
+    .pattern(/[a-zA-Z0-9]/)
+    .required(),
 });
 
 export const registerSchema = Joi.object({
   username: Joi.string().alphanum().min(3).max(20).required(),
   email: Joi.string().email().required,
-  password: Joi.string().min(6).pattern(/[A-Z]/).required(),
+  password: Joi.string()
+    .min(6)
+    .pattern(/[a-zA-Z0-9]/)
+    .required(),
 });
 
 export const newArticleSchema = Joi.object({
